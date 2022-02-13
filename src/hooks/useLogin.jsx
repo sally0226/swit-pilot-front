2d5bf1600b2b1eed01a8f7b5ee3832972707053b
LeeMir <manofgodmj@naver.com>
2022-02-13 17:12:23 +0900

    [Chore] useLogin 예시 작성(주석)

--- src/hooks/useLogin.jsx
+++ src/hooks/useLogin.jsx
@@ -1,6 +1,11 @@
+import { useNavigate } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
+import userState from '../stores/user';
 import fetchApi from '../utils/fetch';
 
 const useLogin = (id, pw) => {
+  const setUser = useSetRecoilState(userState);
+  const navigate = useNavigate();
 
   const login = async () => {
     console.log(id, pw);
@@ -12,6 +17,8 @@ const useLogin = (id, pw) => {
       // 사용자 정보 상태에 저장
       // 메인 페이지로 redirect
       const data = await res.json();
+      setUser(data);
+      navigate('/main');
       console.log('로그인 성공');
     }
     */
