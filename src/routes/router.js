abc76e5e16a5004b12a90538cb12fb5cc0ab16e6
LeeMir <manofgodmj@gmail.com>
2022-02-15 15:39:28 +0900

    [Feat] /main 경로 추가

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
+import MainPage from '../pages/MainPage';
 import SignupPage from '../pages/SignupPage';
 
 const Router = () => {
@@ -9,6 +10,7 @@ const Router = () => {
       <Routes>
         <Route path='/' element={<LoginPage />} />
         <Route path='/signup' element={<SignupPage />} />
+        <Route path='/main' element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   );
