898fc74871e8cad97170b14c77ee6f88e249c24e
LeeMir <manofgodmj@gmail.com>
2022-02-15 15:44:42 +0900

    [Feat] 채팅방 본체(Board) 구성

---
+++ src/components/ChatRoom/ChatBoard/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Message from '../Message';
+import { Container } from './style';
+
+const ChatBoard = ({ messages, lastMessageRef }) => {
+  return (
+    <Container>
+      { messages.map(message => <Message key={message.createdAt} user_name={message.name} contents={message.contents} date={message.createdAt} />)}
+      <div ref={lastMessageRef} />
+    </Container>
+  );
+};
+
+export default ChatBoard;
---
+++ src/components/ChatRoom/ChatBoard/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
+  background-color: #F6F8FA;
+`;
