e3eb59e15695d7d19b9fe91ce8439f4aad2e368c
LeeMir <manofgodmj@gmail.com>
2022-02-15 16:18:18 +0900

    [Feat] 채널 생성 버튼 추가

--- src/components/Channels/index.js
+++ src/components/Channels/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Channel from './Channel';
-import { Container, Title } from './style';
+import { Button, Container, Title, Wrapper } from './style';
 
 const Channels = ({ channels, current }) => {
   return (
     <Container>
-      <Title>Channels</Title>
+      <Wrapper>
+        <Title>Channels</Title>
+        <Button>
+          <img src='/img/plus.svg' alt='plus-icon' />
+        </Button>
+      </Wrapper>
       { channels.map((channel) => <Channel key={channel.id} name={channel.name} isHere={channel.id === current} />) }
     </Container>
   );
--- src/components/Channels/style.js
+++ src/components/Channels/style.js
@@ -6,10 +6,39 @@ export const Container = styled.div`
   flex-direction: column;
   padding: 1rem;
   gap: 0.2rem;
-  border: 1px solid #E6E6E8;
+  border-right: 1px solid #E6E6E8;
   background-color: #FFFFFF;
 `;
 
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 3rem;
+`;
+
 export const Title = styled.span`
-  line-height: 3rem;
+`;
+
+export const Button = styled.button`
+  display: flex;
+  width: 1.5rem;
+  height: 1.5rem;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  border-radius: 50%;
+  background-color: #E6E6E8;
+  opacity: 0.5;
+  cursor: pointer;
+
+  img {
+    width: 0.9rem;
+    height: 0.9rem;
+    filter: invert(52%) sepia(13%) saturate(0%) hue-rotate(306deg) brightness(100%) contrast(98%); // #888888
+  }
+
+  &:hover {
+    opacity: 1;
+  }
 `;
