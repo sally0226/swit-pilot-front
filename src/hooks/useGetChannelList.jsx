28cb082f1cffb730ea23f117b0e3bd85fc0ed16f
LeeMir <manofgodmj@gmail.com>
2022-02-16 16:58:03 +0900

    [Feat] useGetChannelList 작성
    ChannelList 받아온 후, 가장 첫 채널로 이동(moveChannel)하는 customHook

---
+++ src/hooks/useGetChannelList.jsx
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { channelListState } from '../stores/channel';
+import userState from '../stores/user';
+import fetchApi from '../utils/fetch';
+import useMoveChannel from './useMoveChannel';
+
+const channel_dummy = [
+  { channelId: '1', channelName: 'Backend' },
+  { channelId: '2', channelName: 'Frontend' },
+  { channelId: '3', channelName: 'General' },
+];
+
+const useGetChannelList = () => {
+  const [channelList, setChannelList] = useRecoilState(channelListState);
+  const user = useRecoilValue(userState);
+  const moveChannel = useMoveChannel();
+
+  const getChannelList = async () => {
+    /*
+    const res = await fetchApi.get(`/api/channels?email=${user.email}`);
+    if (res.status === 200) {
+      // 데이터 가져오기 성공 시
+      const channels = await res.json();
+      setChannelList(channels);
+      console.log('로그인 성공');
+    }
+    */
+    setChannelList(channel_dummy);
+  }
+
+  useEffect(() => {
+    getChannelList();
+  }, []);
+
+  useEffect(() => {
+    if (channelList.length !== 0) {
+      moveChannel(channel_dummy[0].channelId);
+    }
+  }, [channelList]);
+
+  return channelList;
+};
+
+export default useGetChannelList;
