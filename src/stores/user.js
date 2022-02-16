3580ab2bdb191f7b6be07e9907eceef37e9a32c6
LeeMir <manofgodmj@gmail.com>
2022-02-16 16:59:56 +0900

    [Chore] userState default 값 변경
    pk가 이메일임에 따라 변경

--- src/stores/user.js
+++ src/stores/user.js
@@ -2,7 +2,7 @@ import { atom } from 'recoil';
 
 const userState = atom({
   key: 'userState',
-  default: { id: -1, name: 'LeeMir' },
+  default: { id: '123@gmail.com', name: 'LeeMir' },
 });
 
 export default userState;
