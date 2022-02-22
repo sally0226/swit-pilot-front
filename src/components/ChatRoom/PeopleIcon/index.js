75ef5b2e7e702450271ee68d7b83cf55b9b41fd7
LeeMir <manofgodmj@gmail.com>
2022-02-22 19:32:14 +0900

    [Chore] 이름 변경
    People => PeopleIcon으로 변경

--- src/components/ChatRoom/PeopleIcon/index.js
+++ src/components/ChatRoom/PeopleIcon/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Wrapper } from './style';
 
-const People = ({ num }) => {
+const PeopleIcon = ({ num, toggleUserList }) => {
   return (
-    <Wrapper>
+    <Wrapper onClick={toggleUserList}>
       <img src='/img/people.svg' alt='people-icon' />
       { num }
     </Wrapper>
   );
 };
 
-export default People;
+export default PeopleIcon;
--- src/components/ChatRoom/PeopleIcon/style.js
+++ src/components/ChatRoom/PeopleIcon/style.js
@@ -2,8 +2,14 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   display: flex;
+  padding: 0.5rem;
   gap: 0.5rem;
+  cursor: pointer;
   img {
     filter: invert(52%) sepia(13%) saturate(0%) hue-rotate(306deg) brightness(100%) contrast(98%); // #888888
+  }
+
+  &:hover {
+    background-color: #E6E6E8;
   }
 `;
