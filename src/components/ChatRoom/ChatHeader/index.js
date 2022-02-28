c6a858b3032c9777b4b37b891e97a4b59a0729d7
LeeMir <manofgodmj@gmail.com>
2022-02-28 20:15:37 +0900

    [Feat] 채팅방 나가기, 채팅방 설정 버튼 추가

--- src/components/ChatRoom/ChatHeader/index.js
+++ src/components/ChatRoom/ChatHeader/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { currentChannelState } from '../../../stores/channel';
+import userState from '../../../stores/user';
 import PeopleIcon from '../PeopleIcon';
-import { Title, Wrapper } from './style';
+import { Title, Container, Wrapper, SettingIcon, ExitIcon } from './style';
 
 const ChatHeader = ({ name, people, toggleUserList }) => {
+  const user = useRecoilValue(userState);
+  const channel = useRecoilValue(currentChannelState);
   return (
-    <Wrapper>
-      <Title>{name}</Title>
-      <PeopleIcon num={people.length} toggleUserList={toggleUserList} />
-    </Wrapper>
+    <Container>
+      <Wrapper>
+        <Title>{name}</Title>
+        {
+          (user.userEmail === channel.ownerEmail) &&
+          <SettingIcon>
+            <img src='/img/setting.svg' alt='setting-icon' />
+          </SettingIcon>
+        }
+      </Wrapper>
+      <Wrapper>
+        <PeopleIcon num={people.length} toggleUserList={toggleUserList} />
+        <ExitIcon>
+          <img src='/img/exit.svg' alt='exit-icon' />
+        </ExitIcon>
+      </Wrapper>
+    </Container>
   );
 };
 
--- src/components/ChatRoom/ChatHeader/style.js
+++ src/components/ChatRoom/ChatHeader/style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div`
+export const Container = styled.div`
   display: flex;
   width: calc(100% - 2rem);
   height: 2rem;
@@ -9,6 +9,40 @@ export const Wrapper = styled.div`
   justify-content: space-between;
   border-bottom: 1px solid #E6E6E8;
   background-color: #FFFFFF;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const SettingIcon = styled.div`
+  padding: 0.5rem;
+  cursor: pointer;
+  img {
+    filter: invert(52%) sepia(13%) saturate(0%) hue-rotate(306deg) brightness(100%) contrast(98%); // #888888
+  }
+
+  &:hover {
+    img {
+      filter: invert(0%) sepia(100%) saturate(17%) hue-rotate(53deg) brightness(95%) contrast(100%);
+    }
+  }
+`;
+
+export const ExitIcon = styled.div`
+  padding: 0.5rem;
+  cursor: pointer;
+  img {
+    width: 1.5rem;
+    filter: invert(52%) sepia(13%) saturate(0%) hue-rotate(306deg) brightness(100%) contrast(98%);
+  }
+  &:hover {
+    img {
+      filter: invert(0%) sepia(100%) saturate(17%) hue-rotate(53deg) brightness(95%) contrast(100%);
+    }
+  }
 `;
 
 export const Title = styled.span`
